adafbec30294bc01d0ac4dc3f898a5064331484b
OGURA DAIKI <dkog2001@gmail.com>
2026-06-28 05:10:31 +0900

    ツリー表示　アイコンとクリック判定を追加

--- lib/src/component/widget_tree_editor/widget_tree_editor.dart
+++ lib/src/component/widget_tree_editor/widget_tree_editor.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:jyanken_app_drills/src/model/widget_args_definition/widget_args_wrapper.dart';
 import 'package:jyanken_app_drills/src/model/widget_entity.dart';
 
-class WidgetTreeEditor extends StatefulWidget {
+class WidgetTreeEditor extends StatelessWidget {
+  final int depth;
   final WidgetEntity entity;
   final void Function(WidgetEntity? newEntity) onChange;
 
@@ -11,63 +14,122 @@ class WidgetTreeEditor extends StatefulWidget {
     super.key,
     required this.entity,
     required this.onChange,
+    this.depth = 0,
   });
 
   @override
-  State<WidgetTreeEditor> createState() => _WidgetTreeEditorState();
-}
-
-class _WidgetTreeEditorState extends State<WidgetTreeEditor> {
-  List<String> tree = [];
-
-  @override
-  void initState() {
-    updateTree();
-    super.initState();
-  }
-
-  void updateTree() {
-    final List<String> result = [];
-    final List<(int, WidgetEntity)> list = [(0, widget.entity)];
-    int i = 0;
-    while (i < list.length) {
-      final idx = i++;
-      final needle = list[idx];
-      final depth = needle.$1;
-      final wrapper = WidgetArgsWrapper.fromWidget(needle.$2);
-      result.add("${"  " * depth}${wrapper.type.name}");
-      final subTrees = wrapper.args.entries.where((e) => e.key.type.hasChild);
-      for (final subTree in subTrees) {
-        result.add("${"  " * depth} ${subTree.key.name}");
-        switch (subTree.key.type) {
-          case .widget:
-            list.addAll(switch (subTree.value) {
-              WidgetEntity e => [(depth + 1, e)],
-              _ => [],
-            });
-          case .widgetList:
-            list.addAll(switch (subTree.value) {
-              List<WidgetEntity> l => l.map((e) => (depth + 1, e)),
-              _ => [],
-            });
-          default:
-            break;
-        }
-      }
-    }
-
-    setState(() {
-      tree = result;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final wrapper = WidgetArgsWrapper.fromWidget(entity);
+    final subTree = wrapper.args.entries.where((e) => e.key.type.hasChild);
+
     return Column(
       crossAxisAlignment: .stretch,
       mainAxisSize: .min,
       mainAxisAlignment: .start,
-      children: tree.map((e) => Text(e)).toList(),
+      children: [
+        InkWell(
+          child: Padding(
+            padding: .only(
+              left: 8 + 8 * depth.toDouble(),
+              right: 8,
+              top: 8,
+              bottom: 8,
+            ),
+            child: Row(
+              mainAxisSize: .max,
+              mainAxisAlignment: .start,
+              crossAxisAlignment: .center,
+              spacing: 4,
+              children: [
+                switch (entity.type) {
+                  .text => Icon(Icons.label),
+                  .column => Container(
+                    transform: .rotationZ(pi / 2),
+                    transformAlignment: .center,
+                    child: Icon(Icons.view_column),
+                  ),
+                  .center => Icon(Icons.center_focus_strong),
+                },
+                Text(entity.type.name),
+              ],
+            ),
+          ),
+          onTap: () {},
+        ),
+        ...subTree.map((e) {
+          return Column(
+            crossAxisAlignment: .stretch,
+            mainAxisSize: .min,
+            mainAxisAlignment: .start,
+            children: [
+              ...switch (e.value) {
+                WidgetEntity we => [
+                  WidgetTreeEditor(
+                    depth: depth + 1,
+                    entity: we,
+                    onChange: (newEntity) {
+                      final newArgs = {...wrapper.args};
+                      newArgs[e.key] = newEntity;
+                      onChange(
+                        WidgetEntity.fromArgsWrapper(
+                          wrapper.copyWith(args: newArgs),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+                List<WidgetEntity> wel => [
+                  ...wel.indexed.map((entry) {
+                    final idx = entry.$1;
+                    final we = entry.$2;
+                    return WidgetTreeEditor(
+                      depth: depth + 1,
+                      entity: we,
+                      onChange: (newEntity) {
+                        final newArgs = {...wrapper.args};
+                        List<WidgetEntity?> newList = [...newArgs[e.key]];
+                        newList[idx] = newEntity;
+                        newArgs[e.key] = newList
+                            .whereType<WidgetEntity>()
+                            .toList();
+                        onChange(
+                          WidgetEntity.fromArgsWrapper(
+                            wrapper.copyWith(args: newArgs),
+                          ),
+                        );
+                      },
+                    );
+                  }),
+
+                  TextButton(
+                    onPressed: () {
+                      final newArgs = {...wrapper.args};
+                      List<WidgetEntity?> newList = [...newArgs[e.key]];
+                      newList.add(
+                        .text(
+                          args: .initial.copyWith(
+                            text: "${Random().nextInt(99999)}",
+                          ),
+                        ),
+                      );
+                      newArgs[e.key] = newList
+                          .whereType<WidgetEntity>()
+                          .toList();
+                      onChange(
+                        WidgetEntity.fromArgsWrapper(
+                          wrapper.copyWith(args: newArgs),
+                        ),
+                      );
+                    },
+                    child: Text("追加する"),
+                  ),
+                ],
+                _ => [Text("空")],
+              },
+            ],
+          );
+        }),
+      ],
     );
   }
 }
