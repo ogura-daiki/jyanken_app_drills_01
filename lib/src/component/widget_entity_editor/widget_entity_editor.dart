acd80cecb9e178be5dffeb62048959eb4d2568bf
OGURA DAIKI <dkog2001@gmail.com>
2026-06-28 03:36:43 +0900

    widget_entity_editorの見た目を整えた

--- lib/src/component/widget_entity_editor/widget_entity_editor.dart
+++ lib/src/component/widget_entity_editor/widget_entity_editor.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:jyanken_app_drills/src/component/arg_editor/arg_editor.dart';
 import 'package:jyanken_app_drills/src/model/widget_args_definition/widget_args_wrapper.dart';
@@ -18,20 +19,48 @@ class WidgetEntityEditor extends StatelessWidget {
     final wrapper = WidgetArgsWrapper.fromWidget(entity);
     final keys = wrapper.args.keys.toList();
     return ListView.builder(
-      itemCount: keys.length,
+      itemCount: keys.length + 1,
       itemBuilder: (context, index) {
-        final key = keys[index];
+        if (index == 0) {
+          return Padding(
+            padding: const .symmetric(vertical: 8.0, horizontal: 16),
+            child: Text(
+              entity.type.name,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          );
+        }
+        final key = keys[index - 1];
         final arg = wrapper.args[key];
-        return ArgEditor(
-          arg: key.type,
-          value: arg,
-          onChange: (newValue) {
-            final newArgs = {...wrapper.args};
-            newArgs[key] = newValue;
-            onChange(
-              WidgetEntity.fromArgsWrapper(wrapper.copyWith(args: newArgs)),
-            );
-          },
+        return Card(
+          child: Padding(
+            padding: const .all(8.0),
+            child: Column(
+              spacing: 2,
+              crossAxisAlignment: .start,
+              mainAxisSize: .min,
+              mainAxisAlignment: .start,
+              children: [
+                Text(key.name),
+                Padding(
+                  padding: const .only(left: 8),
+                  child: ArgEditor(
+                    arg: key.type,
+                    value: arg,
+                    onChange: (newValue) {
+                      final newArgs = {...wrapper.args};
+                      newArgs[key] = newValue;
+                      onChange(
+                        WidgetEntity.fromArgsWrapper(
+                          wrapper.copyWith(args: newArgs),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
