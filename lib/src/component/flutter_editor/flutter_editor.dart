9b121d8aa648e80c47a6598cad39115f48c2857e
OGURA DAIKI <dkog2001@gmail.com>
2026-06-29 00:14:37 +0900

    ScreenEntityTest内の内容をFlutterEditorとして分離

---
+++ lib/src/component/flutter_editor/flutter_editor.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:jyanken_app_drills/src/component/widget_catalog/widget_catalog.dart';
+import 'package:jyanken_app_drills/src/component/widget_entity_editor/widget_entity_editor.dart';
+import 'package:jyanken_app_drills/src/component/widget_entity_widget/widget_entity_widget.dart';
+import 'package:jyanken_app_drills/src/component/widget_tree_editor/selection_node.dart';
+import 'package:jyanken_app_drills/src/component/widget_tree_editor/widget_tree_editor.dart';
+import 'package:jyanken_app_drills/src/model/widget_entity.dart';
+
+class FlutterEditor extends StatefulWidget {
+  const FlutterEditor({super.key});
+
+  @override
+  State<FlutterEditor> createState() => _FlutterEditorState();
+}
+
+class _FlutterEditorState extends State<FlutterEditor> {
+  late final ValueNotifier<WidgetEntity?> tree;
+  late final ValueNotifier<SelectionNode?> selection;
+
+  @override
+  void initState() {
+    tree = ValueNotifier(null);
+    selection = .new(null);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: .stretch,
+      mainAxisSize: .max,
+      children: [
+        Expanded(
+          child: Material(
+            clipBehavior: .antiAliasWithSaveLayer,
+            child: Column(
+              crossAxisAlignment: .stretch,
+              children: [
+                Expanded(
+                  child: ValueListenableBuilder(
+                    valueListenable: tree,
+                    builder: (context, value, child) {
+                      return SingleChildScrollView(
+                        padding: const .only(bottom: 48, top: 16),
+                        child: WidgetTreeEditor(
+                          entity: value,
+                          onSelection: (newSelection) {
+                            selection.value = newSelection;
+                          },
+                          onChange: (WidgetEntity? newEntity) {
+                            tree.value = newEntity;
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                WidgetCatalog(),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: 1,
+          color: Theme.of(context).colorScheme.outlineVariant,
+        ),
+        Expanded(
+          child: ValueListenableBuilder(
+            valueListenable: selection,
+            builder: (context, value, child) {
+              if (value == null) {
+                return Center(child: Text("ウィジェットがありません"));
+              }
+              return WidgetEntityEditor(
+                key: ValueKey(value.entity.id),
+                selector: value.selector,
+                initialValue: value.entity,
+                onChange: (newVal) {
+                  value.onChange(newVal);
+                },
+              );
+            },
+          ),
+        ),
+        Container(
+          width: 1,
+          color: Theme.of(context).colorScheme.outlineVariant,
+        ),
+        Padding(
+          padding: const .all(16),
+          child: AspectRatio(
+            aspectRatio: 9 / 19,
+            child: Material(
+              elevation: 4,
+              clipBehavior: .antiAliasWithSaveLayer,
+              child: ValueListenableBuilder(
+                valueListenable: tree,
+                builder: (context, value, child) {
+                  return WidgetEntityWidget(entity: value);
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
