7b4c4707a58b5d7ed8a2b755737b2cd56a0b1c44
OGURA DAIKI <dkog2001@gmail.com>
2026-09-09 02:53:08 +0900

    タッチデバイス　ウィジェットツリーのメニューがツリーのヘッダー以外をタッチで隠れるようにした

--- lib/src/component/widget_tree_editor/widget_tree_header.dart
+++ lib/src/component/widget_tree_editor/widget_tree_header.dart
@@ -25,84 +25,90 @@ class WidgetTreeHeader extends HookWidget {
   Widget build(BuildContext context) {
     final showDropDown = useState(false);
     final focusNode = useFocusNode();
-    return InkWell(
-      focusNode: focusNode,
-      onFocusChange: (value) {
-        showDropDown.value = value;
+    return TapRegion(
+      consumeOutsideTaps: false,
+      onTapOutside: (_) {
+        showDropDown.value = false;
       },
-      onHover: (value) {
-        showDropDown.value = focusNode.hasFocus || value;
-      },
-      onTapDown: (details) {
-        showDropDown.value = true;
-      },
-      onTap: onSelect,
-      child: Padding(
-        padding: .only(
-          left: 8 + 8 * depth.toDouble(),
-          right: 4,
-          top: 0,
-          bottom: 0,
-        ),
-        child: Row(
-          mainAxisSize: .max,
-          mainAxisAlignment: .start,
-          crossAxisAlignment: .center,
-          spacing: 4,
-          children: [
-            WidgetTypeIcon(
-              type: type,
-              size: TextTheme.of(context).bodyMedium?.lineHeight(),
-              color: ColorScheme.of(context).onSurface,
-            ),
-            Expanded(
-              child: Text(
-                type.name,
-                overflow: .ellipsis,
-                style: TextTheme.of(context).bodyMedium?.copyWith(
-                  color: ColorScheme.of(context).onSurface,
+      child: InkWell(
+        focusNode: focusNode,
+        onFocusChange: (value) {
+          showDropDown.value = value;
+        },
+        onHover: (value) {
+          showDropDown.value = focusNode.hasFocus || value;
+        },
+        onTapDown: (details) {
+          showDropDown.value = true;
+        },
+        onTap: onSelect,
+        child: Padding(
+          padding: .only(
+            left: 8 + 8 * depth.toDouble(),
+            right: 4,
+            top: 0,
+            bottom: 0,
+          ),
+          child: Row(
+            mainAxisSize: .max,
+            mainAxisAlignment: .start,
+            crossAxisAlignment: .center,
+            spacing: 4,
+            children: [
+              WidgetTypeIcon(
+                type: type,
+                size: TextTheme.of(context).bodyMedium?.lineHeight(),
+                color: ColorScheme.of(context).onSurface,
+              ),
+              Expanded(
+                child: Text(
+                  type.name,
+                  overflow: .ellipsis,
+                  style: TextTheme.of(context).bodyMedium?.copyWith(
+                    color: ColorScheme.of(context).onSurface,
+                  ),
                 ),
               ),
-            ),
-            Opacity(
-              opacity: showDropDown.value ? 1 : 0,
-              child: IgnorePointer(
-                ignoring: !showDropDown.value,
-                child: PopupMenuButton(
-                  position: .under,
-                  itemBuilder: (context) => WidgetTreeActionType.values
-                      .map(
-                        (v) => PopupMenuItem(
-                          value: v,
-                          enabled: type.isAllowedAction(v),
-                          child: Row(
-                            spacing: 4,
-                            children: [
-                              WidgetTreeDropdownMenuIcon(item: v),
-                              Expanded(
-                                child: WidgetTreeDropdownMenuLabel(item: v),
-                              ),
-                            ],
+              Opacity(
+                opacity: showDropDown.value ? 1 : 0,
+                child: IgnorePointer(
+                  ignoring: !showDropDown.value,
+                  child: PopupMenuButton(
+                    position: .under,
+                    itemBuilder: (context) => WidgetTreeActionType.values
+                        .map(
+                          (v) => PopupMenuItem(
+                            value: v,
+                            enabled: type.isAllowedAction(v),
+                            child: Row(
+                              spacing: 4,
+                              children: [
+                                WidgetTreeDropdownMenuIcon(item: v),
+                                Expanded(
+                                  child: WidgetTreeDropdownMenuLabel(item: v),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                      .toList(),
-                  onSelected: (v) {
-                    switch (v) {
-                      case .editAttribute:
-                        onSelect();
-                      case .changeWidget:
-                        ScaffoldMessenger.of(
-                          context,
-                        ).showSnackBar(SnackBar(content: Text(v.name)));
-                      case .remove:
-                        onRemove();
-                    }
-                  },
+                        )
+                        .toList(),
+                    onSelected: (v) {
+                      switch (v) {
+                        case .editAttribute:
+                          onSelect();
+                        case .changeWidget:
+                          ScaffoldMessenger.of(
+                            context,
+                          ).showSnackBar(SnackBar(content: Text(v.name)));
+                        case .remove:
+                          onRemove();
+                      }
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
