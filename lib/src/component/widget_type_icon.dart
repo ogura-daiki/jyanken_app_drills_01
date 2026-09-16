24c220cf9551ee93781d3d1063d5a11a3cc5daae
OGURA DAIKI <dkog2001@gmail.com>
2026-09-17 05:41:57 +0900

    scopeのアイコンを変更

--- lib/src/component/widget_type_icon.dart
+++ lib/src/component/widget_type_icon.dart
@@ -24,7 +24,7 @@ class WidgetTypeIcon extends StatelessWidget {
         .expanded => const Icon(Icons.expand),
         .center => const Icon(Icons.center_focus_strong),
         .align => const Icon(Icons.center_focus_weak),
-        .scope => const Icon(Icons.change_circle),
+        .scope => const Icon(Icons.data_object),
         .q2_1 || .q2_2 || .q2_3 => const Icon(Icons.question_mark),
       },
     );
